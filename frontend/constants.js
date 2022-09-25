1f29285e9f246ef1a71b32566a4698b339dc2dd1
m <13090422+marciob@users.noreply.github.com>
2022-09-24 22:45:25 -0300

    first commit

---
+++ frontend/constants.js
@@ -0,0 +1,4 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/haardikk21/celo-nft-marketplace";
+
+export const MARKETPLACE_ADDRESS = "0x88642180939b54162C808176EBC65e06eEbf4c65";
